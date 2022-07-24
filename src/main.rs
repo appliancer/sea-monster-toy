4a0782b3b51069b49cb14c0d794a271dafd09241
Peter Fajdiga <peterfaj+linkedin@gmail.com>
2022-07-24 21:52:20 +0200

    parse transaction

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{Read, Write};
 use std::{env, io, process};
 
 fn main() {
@@ -14,20 +14,60 @@ fn main() {
         process::exit(1);
     });
 
-    let reader = BufReader::new(file);
-
-    process_transactions(reader, &mut io::stdout()).unwrap_or_else(|err| {
+    process_transactions(file, &mut io::stdout()).unwrap_or_else(|err| {
         eprintln!("Error processing transactions: {}", err);
         process::exit(1);
     });
 }
 
-fn process_transactions(
-    reader: impl BufRead,
-    writer: &mut impl Write,
-) -> Result<(), Box<dyn Error>> {
-    for line in reader.lines() {
-        writeln!(writer, "{}", line?)?;
+fn process_transactions(reader: impl Read, writer: &mut impl Write) -> Result<(), Box<dyn Error>> {
+    let mut csv_reader = csv::Reader::from_reader(reader);
+    for record in csv_reader.records() {
+        let record = record?;
+        let fields: Vec<&str> = record.iter().map(str::trim).collect();
+        let transaction = parse_transaction(&fields)?;
+        writeln!(writer, "{:?}", transaction)?;
     }
     Ok(())
+}
+
+#[derive(Debug)]
+enum Transaction {
+    Deposit { id: u32, client: u16, amount: f64 },
+    Withdrawal { id: u32, client: u16, amount: f64 },
+    Dispute { client: u16, deposit: u32 },
+    Resolve { client: u16, deposit: u32 },
+    Chargeback { client: u16, deposit: u32 },
+}
+
+fn parse_transaction(fields: &[&str]) -> Result<Transaction, Box<dyn Error>> {
+    return if let [tx_type, client, tx, amount] = fields {
+        Ok(match *tx_type {
+            "deposit" => Transaction::Deposit {
+                id: tx.parse()?,
+                client: client.parse()?,
+                amount: amount.parse()?,
+            },
+            "withdrawal" => Transaction::Withdrawal {
+                id: tx.parse()?,
+                client: client.parse()?,
+                amount: amount.parse()?,
+            },
+            "dispute" => Transaction::Dispute {
+                client: client.parse()?,
+                deposit: tx.parse()?,
+            },
+            "resolve" => Transaction::Resolve {
+                client: client.parse()?,
+                deposit: tx.parse()?,
+            },
+            "chargeback" => Transaction::Chargeback {
+                client: client.parse()?,
+                deposit: tx.parse()?,
+            },
+            _ => return Err(format!("invalid transaction type: {}", tx_type).into()),
+        })
+    } else {
+        Err("invalid number of fields in CSV line".into())
+    };
 }
