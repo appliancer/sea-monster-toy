937ed802d699efd3b46bde12b537bb77d79d1385
Peter Fajdiga <peterfaj+linkedin@gmail.com>
2022-07-24 21:52:20 +0200

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+use std::{env, io, process};
+
 fn main() {
-    println!("Hello, world!");
+    let filename = env::args().nth(1).unwrap_or_else(|| {
+        eprintln!("Missing filename argument");
+        process::exit(1);
+    });
+
+    let file = File::open(&filename).unwrap_or_else(|err| {
+        eprintln!("Error opening file {}: {}", &filename, err);
+        process::exit(1);
+    });
+
+    let reader = BufReader::new(file);
+
+    process_transactions(reader, &mut io::stdout()).unwrap_or_else(|err| {
+        eprintln!("Error processing transactions: {}", err);
+        process::exit(1);
+    });
+}
+
+fn process_transactions(
+    reader: impl BufRead,
+    writer: &mut impl Write,
+) -> Result<(), Box<dyn Error>> {
+    for line in reader.lines() {
+        writeln!(writer, "{}", line?)?;
+    }
+    Ok(())
 }
